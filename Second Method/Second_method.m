0208cfba41b94560076f72566125b3dddafd45f6
Loner <44742364+boomkalala@users.noreply.github.com>
2018-11-12 21:32:30 +0800

    Add files via upload

--- Second Method/Second_method.m
+++ Second Method/Second_method.m
@@ -1,71 +1,54 @@
 image = imread('lena.jpg');
-image = imnoise(image,'gauss');
-subplot(1,2,1);
-imshow(image);
-image = double(image);
+subplot(2,2,1);imshow(image);
+image = imnoise(image,'gaussian',0,0.01);
+subplot(2,2,2);imshow(image);
 
-%参数设置
-lamda = 25;
+%初始化参数
+image = double(image);
 [M,N] = size(image);
-bita1X = zeros(M,N);
-bita1Y = zeros(M,N);
-bita2 = zeros(M,N);
+u = image;
 Wx = zeros(M,N);
 Wy = zeros(M,N);
 V = zeros(M,N);
-% fai = v.^2;
-% div_fai = 2.*v;
-mu = 0.5;
-u = image;
+bita1X = zeros(M,N);
+bita1Y = zeros(M,N);
+bita2 = zeros(M,N);
+mu = 0.5;lamda = 25;
+total = 100;
 
-
-for step = 1:100
-    %处理u的
-    W = divergence(Wx,Wy);
+%迭代
+for step = 1:total
+    [ux,uy] = gradient(u);
+    Au = center_diff(u);
+    div_w  = divergence(Wx,Wy);
     div_bita1 = divergence(bita1X,bita1Y);
-    [Ux,Uy]=gradient(u);
-    
     %更新bita
-    bita1X = bita1X + mu*(W - Ux);
-    bita1Y = bita1Y + mu*(W - Uy);
-    bita2 = bita2 + mu*(V - W);
-    
-    F1 = image - mu*W - div_bita1;
-    u = center_diff(u);
-    u = (F1 + mu*u)./(1+4*mu);
-    
-    %对新的u求梯度
-    [Ux,Uy] = gradient(u);
+    bita1X = bita1X + mu*(Wx-ux);
+    bita1Y = bita1Y + mu*(Wy-uy);
+    bita2 = bita2 + mu*(V - div_w);
+    u = (image + mu*Au - mu*div_w - div_bita1) ./ (1 + 4*mu);
+    [ux,uy] = gradient(u);
+    [b2x,b2y] = gradient(bita2);
     [Vx,Vy] = gradient(V);
-    [B2x,B2y] = gradient(bita2);
-    F2x = -mu*Ux + bita1X + mu*Vx + B2x;
-    F2y = -mu*Uy + bita1Y + mu*Vy + B2y;
-    
-    %对W
+    %迭代Wx,Wy
     AW1 = center_diff(Wx);
     AW2 = center_diff(Wy);
-    Wx = (mu*AW1 - F2x) ./ (5*mu);
-    Wy = (mu*AW2 - F2y) ./ (5*mu);
-    
-    %更新V
-    W = divergence(Wx,Wy);
-    V = max(abs(W - bita2/mu) - lamda/mu,0) .* (W - bita2*mu) ./ abs(W - bita2*mu);
-%     V =  max(abs(W - bita2/mu) - lamda.*div_fai/mu,0) .* (W - bita2*mu) ./ abs(W - bita2*mu);
-%     [f1 f2 Af2]=grad(fai);
-%     abs_fai=sqrt(f1.^2+f2.^2);
-%     E(step)=sum(sum(lamda.*abs_fai+0.5.*(u-image).^2));    
-    u2 = divergence(Ux,Uy);
-%     E(step)=0.5*sum(sum((u-image).^2))+lamda*sum(sum(Ux.^2+Uy.^2));
-    E(step) = sum(lamda*sum(abs(u2)) + 0.5*sum(u-image).^2);
-    if step > 2
-        abs(E(step) - E(step-1) / E(step)) < 0.001;
-        break;
+    Wx = (mu*ux - bita1X - b2x - mu*Vx + mu*AW1)./(5*mu);
+    Wy = (mu*uy - bita1Y - b2y - mu*Vy + mu*AW2)./(5*mu);
+    div_w = divergence(Wx,Wy);
+    V = max(div_w - bita2/mu,0) .* (div_w - bita2/mu) ./ abs(div_w - bita2/mu + eps);
+    E(step) = sum(lamda*sum(abs(divergence(ux,uy))) + 0.5*sum((u-image).^2));
+    if (step>2)
+        if abs((E(step)-E(step-1))/E(step))<0.001
+            break;
+        end
     end
 end
-subplot(1,2,2);
-imshow(uint8(u));
 
 
+subplot(2,2,3);imshow(uint8(u));title('result');
+subplot(2,2,4);plot(E);xlabel('Iterations');ylabel('Energy');legend('Energy/Iterations');
+PSNR=psnr(u,image)
 
 
 
--- Second Method/center_diff.m
+++ Second Method/center_diff.m
@@ -1,10 +1,10 @@
 function u = center_diff(u)
     [M,N] = size(u);
-    left = circshift(u, [0,-1]);left(:,M) = left(:,M-1);
+    left = circshift(u, [0,-1]);left(:,N) = left(:,N-1);
     right = circshift(u, [0,1]); right(:,1) = right(:,2);
-    up = circshift(u, [1,0]);
-    up(N,:) = up(N-1,:);
-    down = circshift(u, [-1,0]);
+    up = circshift(u, [-1,0]);
+    up(M,:) = up(M-1,:);
+    down = circshift(u, [1,0]);
     down(1,:) = down(2,:);
     
     u = left + right + up + down;
